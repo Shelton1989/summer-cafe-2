3897f8280570e91ecd1c32ca86a4ae4f084e6442
Shelton Wilson <sheltonwlsn@gmail.com>
2019-06-27 22:34:51 +0200

    Added resizable panels to main content window

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ import {
   ReflexContainer,
   ReflexElement,
   ReflexSplitter
-} from 'react-reflex'
+} from 'react-reflex';
+
+import 'react-reflex/styles.css';
 
 function App() {
   return (
@@ -28,34 +30,23 @@ function App() {
 function Panes() {
   return (
     <div className='panes'>
-       <ReflexContainer orientation="vertical">
-
-<ReflexElement className="left-pane">
-  <div className="pane-content">
-    <label>
-      Left Pane (resizable)
-    </label>
-  </div>
-</ReflexElement>
-
-<ReflexSplitter propagate={true}/>
-
-<ReflexElement className="right-pane"
-  minSize="200"
-  maxSize="800">
-  <div className="pane-content">
-    <label>
-      Right Pane (resizable)
-      <br/>
-      <br/>
-      minSize = 200px
-      <br/>
-      maxSize = 800px
-    </label>
-  </div>
-</ReflexElement>
-
-</ReflexContainer>
+      <ReflexContainer orientation='vertical'>
+        <ReflexElement className='explorer' flex={0.1}>
+          <div>Test</div>
+        </ReflexElement>
+        <ReflexSplitter propagate={true} />
+        <ReflexElement className='content' flex={0.9}>
+          <ReflexContainer orientation='horizontal'>
+            <ReflexElement className='MainPanel' flex={0.8}>
+              <div>MainPanel</div>
+            </ReflexElement>
+            <ReflexSplitter propagate={true} />
+            <ReflexElement className='Terminal'>
+              <div>Terminal</div>
+            </ReflexElement>
+          </ReflexContainer>
+        </ReflexElement>
+      </ReflexContainer>
     </div>
   )
 }
